5665a93f40dd1eebfdabd4dac0f87224cb2e1df1
= <tristanmayofreire@gmail.com>
2023-12-06 09:03:05 +0100

    ci

---
+++ ts-up.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  target: "es2015",
+  format: ["cjs", "esm"],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  dts: true,
+});
