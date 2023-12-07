2252c027bcb50165d316e21d4df1f9f57f3ea12a
= <tristanmayofreire@gmail.com>
2023-12-07 16:17:06 +0100

    ast diffing v1 + tests

--- src/astDiffer.test.ts
+++ src/astDiffer.test.ts
@@ -1,6 +1,53 @@
 import { beforeEach, describe, expect, it } from "vitest";
-import { diffRule } from "./astDiffer";
+import { astDiff, diffRule } from "./astDiffer";
 import * as postcss from "postcss-js";
+
+describe.skip("AST diff", () => {
+  const lhsO = {
+    "@media (max-width: 300px)": {
+      ".myclass": {
+        background: "red",
+        color: "red",
+        "z-index": "1",
+      },
+      div: {
+        background: "red",
+      },
+      body: {
+        background: "red",
+      },
+    },
+  };
+  const lhs = postcss.parse(lhsO);
+
+  it.skip("Should return an empty array if no diff has been found", () => {
+    const difres: any[] = [];
+
+    astDiff(lhs.first as any, lhs.first as any, difres);
+
+    expect(diffRule).toEqual([]);
+  });
+
+  it.skip("Should handle removal of a single node", () => {
+    const rhsO = { ...lhsO, body: undefined };
+    const difres: any[] = [];
+
+    astDiff(lhs.first, postcss.parse(rhsO).first, difres);
+    expect(difres).toEqual({ type: "node", path: [2], value: null });
+  });
+
+  it.skip("Should handle creation of a single node", () => {});
+
+  it.skip("Should handle removal of two nodes", () => {
+    const difres: any[] = [];
+    const rhsO = { ...lhsO, body: undefined, div: undefined };
+    astDiff(lhs.first, postcss.parse(rhsO).first, difres);
+    expect(difres).toEqual([
+      { type: "node", path: [1], value: null },
+      { type: "node", path: [2], value: null },
+    ]);
+  });
+});
 
 describe("AST Diff Rule", () => {
   const lhsO = {
--- src/astDiffer.ts
+++ src/astDiffer.ts
@@ -1,20 +1,22 @@
 import * as postcss from "postcss-js";
-import type { ASTNode } from "./astInterface";
+import type { ASTNode, ASTNodes } from "./astInterface";
+import type { AST } from "./astInterface";
 
-export type DiffNewNode = { type: "node"; value: postcss.CssInJs };
+export type DiffNode = { type: "node"; value: postcss.CssInJs | null };
 export type DiffProperties = {
   type: "properties";
   value: { [k in string]: string | null };
 };
+export type DiffRaw = { type: "raw"; value: string };
 
-export type Diff = DiffNewNode | DiffProperties;
+export type Diff = DiffNode | DiffProperties | DiffRaw;
 
 // See tests for examples.
 // for node changes returns the rhs node (could update this to allow taking in undefined lhs & rhs)
 // new node
 
 export function diffRule(lhs: ASTNode, rhs: ASTNode) {
-  const diff = [];
+  const diff: Diff[] = [];
   if (lhs.type !== "rule" || rhs.type !== "rule")
     throw new Error("invalid node types passed to diff");
 
@@ -27,14 +29,20 @@ export function diffRule(lhs: ASTNode, rhs: ASTNode) {
     return diff;
   }
 
+  diffProps(lhs.nodes, rhs.nodes, diff);
+
+  return diff;
+}
+
+export function diffProps(lhs: any[], rhs: any[], diff: Diff[]) {
   let propsChanged = false;
   const properties: DiffProperties = { type: "properties", value: {} };
 
   // compare each dcl
-  const maxLen = Math.max(lhs.nodes.length, rhs.nodes.length);
+  const maxLen = Math.max(lhs.length, rhs.length);
   for (let i = 0; i < maxLen; i++) {
-    const rnode: any = rhs.nodes[i];
-    const lnode: any = lhs.nodes[i];
+    const rnode: any = rhs[i];
+    const lnode: any = lhs[i];
     if (!rnode) {
       // removed
       properties.value[lnode.prop] = null;
@@ -59,4 +67,41 @@ export function diffRule(lhs: ASTNode, rhs: ASTNode) {
   }
 
   return diff;
+}
+
+//This is naive implementation since we assume only one "branch" of changes (i.e that we cannot pass from:
+// @media (max-width: 300px) { .myOldClass {} .myClass {}}
+// @media (max-width: 300px) {.myNewClass{}}
+// if there is a big difference between one node and the other we should probably just delete the current node and insert a new one. But that seems a little hard to implement.
+// todo this is complicated
+export function astDiff(
+  lhs: AST | ASTNode | ASTNodes | undefined,
+  rhs: AST | ASTNode | ASTNodes | undefined,
+  result: Diff[]
+) {
+  if (!lhs && !rhs) {
+    return result;
+  }
+  if (!lhs) {
+    // created rhs
+    result.push({ type: "node", value: postcss.objectify(rhs as any) });
+    return result;
+  }
+  if (!rhs) {
+    // deleted lhs
+    result.push({ type: "node", value: null });
+    return result;
+  }
+
+  if (Array.isArray(lhs) && Array.isArray(rhs)) {
+    if (
+      (lhs[0] && lhs[0].type === "decl") ||
+      (rhs[0] && rhs[0].type === "decl")
+    ) {
+    }
+    const max = Math.max(lhs.length, rhs.length);
+    for (let i = 0; i < max; i++) {
+      astDiff(lhs[i], rhs[i], result);
+    }
+  }
 }
